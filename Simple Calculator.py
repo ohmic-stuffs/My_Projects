4c3b0bea8f03c8ceb78adbefe9d6a7d894df1030
ohmic.stuffs <omkarankit2004@gmail.com>
2024-06-24 12:09:04 +0530

    Edited push

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -1,56 +1,155 @@
-# This function adds two numbers
-def add(x, y):
-    return x + y
-
-# This function subtracts two numbers
-def subtract(x, y):
-    return x - y
+"""
+This is a simple python calculator.
+"""
 
 
-# This function multiplies two numbers
-def multiply(x, y):
-    return x * y
-
-# This function divides two numbers
-def divide(x, y):
-    return x / y
+def add():
+    '''adds two numbers'''
+    num1 = int(input("Enter number 1: "))
+    num2 = int(input("Enter number 2: "))
+    print(f"The sum of {num1} and {num2} is {num1 + num2}")
 
 
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
+def subtract():
+    '''subtracts two numbers'''
+    num1 = int(input("Enter number 1: "))
+    num2 = int(input("Enter number 2: "))
+    print(f"The difference of {num1} and {num2} is {num1 - num2}")
 
-while True:
-    # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
 
-    # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
+def multiply():
+    '''multiplies two numbers'''
+    num1 = int(input("Enter number 1: "))
+    num2 = int(input("Enter number 2: "))
+    print(f"The product of {num1} and {num2} is {num1 * num2}")
+
+
+def divide():
+    '''divides two numbers'''
+    num1 = int(input("Enter number 1: "))
+    num2 = int(input("Enter number 2: "))
+    # to check if divided by zero
+    try:
+        print(f"The division of {num1} and {num2} results in {num1 / num2}")
+    except ZeroDivisionError as e:
+        print(e)
+
+
+def exponent():
+    '''exponentiates a number'''
+    num = int(input("Enter number to be exponented: "))
+    power = int(input("Enter power to be raised at: "))
+    print(f"{num} raised to {power} is {num ** power}")
+
+
+def sqrt():
+    '''sqrt -> square root'''
+    num = int(input("Enter number whose square root you want to know: "))
+    print(f"Square root of {num} is {num ** 0.5}")
+
+
+def cbrt():
+    '''cbrt -> cube root'''
+    num = int(input("Enter number whose cube root you want to know: "))
+    print(f"Cube root of {num} is {num ** (1/3)}")
+
+
+def recurse_factor(n):
+    # recursive function to find factorial
+    if n == 1:
+        return n
+    else:
+        return n * recurse_factor(n - 1)
+
+
+def fact():
+    '''finds factorial of a number'''
+    # NOTE- This is recursive and can only calculate till python data bounds
+    num = int(input("Enter the number whose factorial you want to find: "))
+    if num <= 0:
+        print("Factorial doesn't exist for negative numbers" if num < 0 else 'The factorial of 0 is 1')
+    else:
+        print(f"Factorial of {num} is {recurse_factor(num)}")
+
+
+def NTW(n=-1):
+    if(n==-1):
+        n = int(input("Enter the number whose conversion you want to find: "))
+    units = ["", "One", "Two", "Three", "Four",
+            "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve",
+            "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen",
+            "Eighteen", "Nineteen" ]
+    tens = ["","","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"]
+    if (n < 20):
+        return units[n]
+    if (n < 100):
+        return tens[n//10]+(" " if (n % 10 != 0) else "")+units[n % 10]
+    if (n < 1000):
+        return units[n//100]+" Hundred"+(" " if (n%100!= 0) else "")+NTW(n % 100)
+    if (n < 100000):
+        return NTW(n//1000) +" Thousand"+(" " if (n % 10000 != 0) else "")+NTW(n % 1000)
+    if (n < 10000000):
+        return NTW(n//100000)+" Lakh"+(" " if (n % 100000 != 0) else "")+NTW(n % 100000)
+    return NTW(n//10000000)+" Crore"+(" " if (n % 10000000 != 0) else "")+NTW(n % 10000000)
+
+
+
+def main():
+    while True:
+        # Taking input from the user
         try:
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
-        except ValueError:
-            print("Invalid input. Please enter a number.")
+            select = int(input("Please select operation -\n"
+                "1. Add\n"
+                "2. Subtract\n"
+                "3. Multiply\n"
+                "4. Divide\n"
+                "5. Exponentation\n"
+                "6. Square Root\n"
+                "7. Cube Root\n"
+                "8. Factorial\n"
+                "9. Number To Words\n"
+                "10. Quit\n"))
+        except ValueError as e:
+            print(f'{e} Error,\n Invalid Option...')
             continue
 
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+        if select == 1:
+            add()
 
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
+        elif select == 2:
+            subtract()
 
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
+        elif select == 3:
+            multiply()
 
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-        
-        # check if user wants another calculation
-        # break the while loop if answer is no
-        next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
-          break
-    else:
-        print("Invalid Input")
+        elif select == 4:
+            divide()
+
+        elif select == 5:
+            exponent()
+
+        elif select == 6:
+            sqrt()
+
+        elif select == 7:
+            cbrt()
+
+        elif select == 8:
+            fact()
+
+        elif select == 9:
+            print("\n",NTW(),"\n\n\n")
+
+
+        elif select == 10:
+            print("Thanks for visiting!")
+            break
+
+        else:
+            print("Invalid input!")
+
+    print("\n************ Thanks for using the calculator **************\n")
+
+
+if __name__ == "__main__":
+    main()
